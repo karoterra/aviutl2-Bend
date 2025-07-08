2a4210f51db400ed010f8f834ab5925d3fb2ab10
karoterra <karoterra2008@gmail.com>
2025-07-08 23:31:54 +0900

    :hammer: 別々でスクリプトを書いてから結合するように開発用スクリプトを作成

---
+++ tool/build.py
@@ -0,0 +1,61 @@
+# /// script
+# requires-python = ">=3.13"
+# ///
+
+from pathlib import Path
+import shutil
+
+
+def insert_param(text: str, key: str, value: str) -> str:
+    return text.replace(f"${{{key}}}", value)
+
+
+def insert_dictionary(text: str, dictionary: dict) -> str:
+    for key, value in dictionary.items():
+        text = insert_param(text, key, value)
+    return text
+
+
+def main():
+    AUTHOR = "karoterra"
+    VERSION = "v0.1.0"
+
+    build_dir = Path("build")
+    build_dir.mkdir(exist_ok=True)
+
+    script_texts = []
+    with open("script/@曲げKR.in.anm2", "r", encoding="utf-8") as f:
+        text = f.read()
+        text = insert_dictionary(text, {
+            "INFO": "曲げKR for AviUtl2",
+            "VERSION": VERSION,
+            "AUTHOR": AUTHOR
+        })
+        script_texts.append(text)
+    with open("script/@曲げKR_円弧.in.anm2", "r", encoding="utf-8") as f:
+        text = f.read()
+        text = insert_dictionary(text, {
+            "INFO": "曲げKR (円弧) for AviUtl2",
+            "VERSION": VERSION,
+            "AUTHOR": AUTHOR
+        })
+        script_texts.append("@円弧")
+        script_texts.append(text)
+    with open("script/@曲げKR_ベジェ曲線.in.anm2", "r", encoding="utf-8") as f:
+        text = f.read()
+        text = insert_dictionary(text, {
+            "INFO": "曲げKR (ベジェ曲線) for AviUtl2",
+            "VERSION": VERSION,
+            "AUTHOR": AUTHOR
+        })
+        script_texts.append("@ベジェ曲線")
+        script_texts.append(text)
+
+    with open("build/@曲げKR.anm2", "w", encoding="utf-8", newline="\r\n") as f:
+        f.write("\n".join(script_texts))
+
+    shutil.copy("script/KaroterraBend.lua", build_dir / "KaroterraBend.lua")
+
+
+if __name__ == "__main__":
+    main()
